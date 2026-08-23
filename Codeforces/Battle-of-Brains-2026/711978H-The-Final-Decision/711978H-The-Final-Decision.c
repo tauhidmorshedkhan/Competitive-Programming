bf069b9a192204ccec12a06900e862c4e8429b84
Tauhid Morshed Khan <tauhidmorshedkhan@gmail.com>
2026-08-23 19:01:22 +0600

    Create 711978H-The-Final-Decision.c

---
+++ Codeforces/Battle-of-Brains-2026/711978H-The-Final-Decision/711978H-The-Final-Decision.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+int main()
+ 
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    if (A == B)
+    {
+    printf("%d", A * B);
+    }
+    else
+    {
+    printf("%d", A + B);
+    }
+    return 0;
+}
