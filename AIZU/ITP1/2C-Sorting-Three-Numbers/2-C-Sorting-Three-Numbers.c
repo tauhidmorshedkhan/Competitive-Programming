5df12f8cc9d1279f22107e16d10ea00ce0fc7faa
Tauhid Morshed Khan <tauhidmorshedkhan@gmail.com>
2026-08-27 15:05:12 +0600

    Create 2-C-Sorting-Three-Numbers.c

---
+++ AIZU/ITP1/2C-Sorting-Three-Numbers/2-C-Sorting-Three-Numbers.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+    long int A, B, C;
+    scanf("%ld %ld %ld", &A, &B, &C);
+    // If A is largest.
+    if (A > B && A > C && B > C)
+    {
+        printf("%ld %ld %ld\n", C, B, A);
+    }
+    else if (A > B && A > C && B == C)
+    {
+        printf("%ld %ld %ld\n", C, B, A);
+    }
+    else if (A > B && A > C && C > B)
+    {
+        printf("%ld %ld %ld\n", B, C, A);
+    }
+    // If B is largest.
+    else if (B > A && B > C && A > C)
+    {
+        printf("%ld %ld %ld\n", C, A, B);
+    }
+    else if (B > A && B > C && A == C)
+    {
+        printf("%ld %ld %ld\n", C, A, B);
+    }
+    else if (B > A && B > C && C > A)
+    {
+        printf("%ld %ld %ld\n", A, C, B);
+    }
+    // If C is largest.
+    else if (C > A && C > B && A > B)
+    {
+        printf("%ld %ld %ld\n", B, A, C);
+    }
+    else if (C > A && C > B && A == B)
+    {
+        printf("%ld %ld %ld\n", B, A, C);
+    }
+    else if (C > A && C > B && B > A)
+    {
+        printf("%ld %ld %ld\n", A, B, C);
+    }
+    return 0;
+}
