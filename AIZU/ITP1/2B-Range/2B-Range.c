46e3718e3547d7724b14d5fc5ef973ef6a71b507
Tauhid Morshed Khan <tauhidmorshedkhan@gmail.com>
2026-08-29 20:19:36 +0600

    Create 2B-Range.c

---
+++ AIZU/ITP1/2B-Range/2B-Range.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    if (a != b)
+    {
+        if (c > a && c > b)
+    printf("Yes\n");
+    else
+    {
+        printf("No\n");
+    }
+    }
+    else
+    {
+        printf("No\n");
+    }
+    return 0;
+}
