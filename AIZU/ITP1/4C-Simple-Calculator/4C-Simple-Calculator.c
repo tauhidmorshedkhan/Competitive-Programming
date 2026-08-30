d32cc96f37f1786f66f25c05b6fbb51e7959f800
Tauhid Morshed Khan <tauhidmorshedkhan@gmail.com>
2026-08-31 00:29:52 +0600

    Create 4C-Simple-Calculator.c

---
+++ AIZU/ITP1/4C-Simple-Calculator/4C-Simple-Calculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    char operator;
+    while(1)
+    {
+        scanf("%d %c %d", &a, &operator, &b);
+        if (operator == '?')
+        {
+            break;
+        }
+        else if (operator == '+')
+        {
+            printf("%d\n", a + b);
+        }
+        else if (operator == '-')
+        {
+            printf("%d\n", a - b);
+        }
+        else if (operator == '*')
+        {
+            printf("%d\n", a * b);
+        }
+        else
+        {
+            printf("%d\n", a / b);
+        }
+    }
+return 0;
+}
