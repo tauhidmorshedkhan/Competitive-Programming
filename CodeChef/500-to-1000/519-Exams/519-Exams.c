cf2d9a1a0c629ee9292fcf4f87b4e48cfd866183
Tauhid Morshed Khan <tauhidmorshedkhan@gmail.com>
2026-09-12 19:40:48 +0600

    Update 519-Exams.c

--- CodeChef/500-to-1000/519-Exams/519-Exams.c
+++ CodeChef/500-to-1000/519-Exams/519-Exams.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main() {
-  int T;
+  	int T;
 	float X, Y, Z, rate;
 	scanf("%d", &T);
 	for (int i = 0; i < T; i++)
-  {
+  	{
       scanf("%f %f %f", &X, &Y, &Z);
       rate = (Z * 100.0) / (X * Y);
       if (rate > 50.0)
@@ -16,6 +16,6 @@ int main() {
       {
           printf("NO\n"); 
       }
-  }
+  	}
   return 0;
 }
